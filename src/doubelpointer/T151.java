a311a6885f159ccd55f7be20e658bdf08452b175
Kiki <1351090471@qq.com>
2022-03-24 09:10:08 +0800

    2022-3-24 first commit

---
+++ src/doubelpointer/T151.java
@@ -0,0 +1,34 @@
+package doubelpointer;
+
+
+
+public class T151 {
+
+    public String reverseWords(String s) {
+        s = s.trim();
+        String[] s1 = s.split(" ");
+        String[] s2 = new String[s1.length];
+        int len = 0;
+        for (String str:
+             s1) {
+            if(!"".equals(str)){
+
+                s2[len++] = str;
+            }
+        }
+        String ret = s2[len - 1];
+
+        for (int i = len - 2; i >= 0; i--) {
+            ret += " ";
+            ret += s2[i];
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        T151 t151 = new T151();
+        String s = t151.reverseWords("  hello   world  ");
+        System.out.println(s);
+    }
+
+}
