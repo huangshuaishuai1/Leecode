45540215b536a58313ea7e2fe97f079932e3f855
huangshuaishuai <1351090471@qq.com>
2023-07-02 17:52:44 +0800

    2023-7-2 commit 广度优先遍历

--- src/graph/T1926.java
+++ src/graph/T1926.java
@@ -1,89 +1,53 @@
 package graph;
 
+import java.util.ArrayDeque;
+
 public class T1926 {
 
     public static void main(String[] args) {
         T1926 t1926 = new T1926();
         char[][] chars = new char[1][2];
-        chars[0] = new char[]{'.','+'};
+        chars[0] = new char[]{'+','.'};
 //        chars[1] = new char[]{'.','.','.'};
 //        chars[2] = new char[]{'+','+','+'};
 
-        System.out.println(t1926.nearestExit(chars, new int[]{0, 0}));
+        System.out.println(t1926.nearestExit(chars, new int[]{0, 1}));
     }
 
-    static int ans = -1;
-    static int f = 0;
+
     public int nearestExit(char[][] maze, int[] entrance) {
-        if (maze.length == 1 && maze[0].length == 1) {
-            return 0;
-        }
-        int[][] used = new int[maze.length][maze[0].length];
+        int[][] dirt = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
-        for (int i = 0; i < maze.length; i++) {
-            for (int j = 0; j < maze[0].length; j++) {
-                if (maze[i][j] == '+') {
-                    used[i][j] = 1;
+        ArrayDeque<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[]{entrance[0],entrance[1]});
+        maze[entrance[0]][entrance[1]] = '+';
+        int ans = 0;
+        while (!queue.isEmpty()) {
+            ans ++;
+            int sz = queue.size();
+            for (int i = 0; i < sz; i++) {
+                // 遍历每一个可以到的格子
+                int[] poll = queue.poll();
+                maze[poll[0]][poll[1]] = '+';
+                for (int j = 0; j < 4; j++) {
+                    if (poll[0] + dirt[j][0] < 0 || poll[0] + dirt[j][0] > maze.length-1
+                    || poll[1] + dirt[j][1] < 0 || poll[1] + dirt[j][1] > maze[0].length-1) {
+                        continue;
+                    }
+                    if (maze[poll[0] + dirt[j][0]][poll[1] + dirt[j][1]] == '.') {
+                        if (poll[0] + dirt[j][0] == 0 || poll[0] + dirt[j][0] == maze.length-1
+                        || poll[1] + dirt[j][1] == 0 || poll[1] + dirt[j][1] == maze[0].length-1) {
+                            return ans;
+                        }
+                        queue.add(new int[]{poll[0] + dirt[j][0],poll[1] + dirt[j][1]});
+                        maze[poll[0] + dirt[j][0]][poll[1] + dirt[j][1]] = '+';
+                    }
                 }
             }
         }
-        dfs(used, entrance);
 
-        if(f == 0) {
-            return -1;
-        }
-        return ans;
+        return -1;
     }
 
-    private static void dfs(int[][] used, int[] entrance) {
-        used[entrance[0]][entrance[1]] = 1;
-        ans++;
 
-        if (ans != 0) {
-            if (entrance[0] == 0 || entrance[0] == used.length - 1 || entrance[1] == 0 || entrance[1] == used[0].length) {
-                f = 1;
-                return;
-            }
-        }
-
-        if (entrance[0] - 1 >= 0) {
-            if (used[entrance[0] - 1][entrance[1]] != 1) {
-                dfs(used, new int[]{entrance[0] - 1, entrance[1]});
-                if (f == 1) {
-                    return;
-                }
-                used[entrance[0] - 1][entrance[1]] = 0;
-            }
-        }
-
-        if (entrance[0] + 1 <= used.length-1) {
-            if (used[entrance[0] + 1][entrance[1]] != 1) {
-                dfs(used, new int[]{entrance[0] + 1, entrance[1]});
-                if (f == 1) {
-                    return;
-                }
-                used[entrance[0] + 1][entrance[1]] = 0;
-            }
-        }
-        if (entrance[1] - 1 >= 0) {
-            if (used[entrance[0] ][entrance[1] - 1] != 1) {
-                dfs(used, new int[]{entrance[0], entrance[1]-1});
-                if (f == 1) {
-                    return;
-                }
-                used[entrance[0]][entrance[1]-1] = 0;
-            }
-        }
-
-        if (entrance[1] + 1 <= used[0].length-1) {
-            if (used[entrance[0]][entrance[1]+1] != 1) {
-                dfs(used, new int[]{entrance[0], entrance[1] + 1});
-                if (f == 1) {
-                    return;
-                }
-                used[entrance[0]][entrance[1] + 1] = 0;
-            }
-        }
-
-    }
 }
