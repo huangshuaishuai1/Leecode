fba158d91a12dc5d90339daefc18bc33d4ec7e33
huangshuaishuai <1351090471@qq.com>
2023-07-20 11:51:54 +0800

    2023-7-18

--- src/array/demo.java
+++ src/array/demo.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 
 public class demo {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = scanner.nextInt();
-            System.out.println(nums[i]);
-        }
+        String str = (3174.0/1419211.0)*100+"";
+        int i = str.indexOf('.');
+        System.out.println(str);
+        System.out.println(str.substring(0,i+3));
 
     }
 
