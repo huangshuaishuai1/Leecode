b58f5c2e346ca4c360e5924372b80452cdf0041c
huangshuaishuai <1351090471@qq.com>
2023-10-03 00:13:31 +0800

    2023-9-27

---
+++ src/bd/StrMulti.java
@@ -0,0 +1,16 @@
+//package bd;
+//
+//public class StrMulti {
+//
+//    static String mul(String a, String b) {
+//        if (a == null || b==null || a.length() == 0 || b.length() == 0) {
+//            return null;
+//        }
+//        int ans = 0;
+//        for (int i = 0; i < a.length(); i++) {
+//
+//        }
+//
+//
+//    }
+//}
